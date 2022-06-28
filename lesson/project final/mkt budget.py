fe52e488253df1eb2b4dd2a1aa7c227c607a9f3e
david <davidzeller33@gmail.com>
2022-06-28 17:43:16 +0300

    tragilim

--- lesson/project final/mkt budget.py
+++ lesson/project final/mkt budget.py
@@ -8,22 +8,20 @@ How long he wants the Instagram campaign will run.
 Then print to the screen the summary of the cost in ILS with tax (17%)
 If it is more than his marketing budget, tell him how much to add, if not tell him "successfull
 '''
-
 num=int(input("Enter how much money do you have for marketing budget: "))
-print("List of campaign prices(within Tax): \nFacebook: 100ILS per day\nInstagram: 50ILS per day")
+print("List of campaign prices(within Tax): \nFacebook: 100 ILS per day\nInstagram: 50 ILS per day")
 a=int(input("How long do you wants the Facebook campaign wiil run?  "))
 b=int(input("How long do you wants the Instagram campaign wiil run?  "))
 
 cost=((((a*100)+(b*50))*1.17))
 
-budget=int(num-cost)
 
-print("--------------\nCost of all campaign: " + str((a*100)+(b*50)) + "ILS\nTax: " + str((((a*100)+(b*50))*0.17) + "ILS\nTotal cost of the campaign: " + str(cost) + "ILS\n------------")
 
-if budget=0 or budget>0:
+print("--------------\nCost of all campaign: " + str((a*100)+(b*50)) + " ILS\nTax(17%): " + str(((a*100)+(b*50))*0.17) + " ILS\nTotal cost of the campaign: " + str(cost) + " ILS\n------------")
+
+budget= int(num-cost)
+
+if budget==0 or budget>0:
     print("successful!!!")
 else:
-    print("Your need to add on your budget " + str(cost - num) + "ILS.")
-
-
-
+    print("Your need to add on your budget " + str(cost - num) + " ILS.")
